60132cb41b1446f36829984456b453594c9a37f7
NgocUyen297 <70994909+NgocUyen297@users.noreply.github.com>
2020-09-12 08:43:05 +0700

    add lien he

--- js/java.js
+++ js/java.js
@@ -1,26 +1,19 @@
-//// banner /////
-var myIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("myslide");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) {
-        myIndex = 1
-    }
-    x[myIndex - 1].style.display = "block";
-    setTimeout(carousel, 2000); // Change image every 2 seconds
-}
-
 /// thanh NAV ////
+
 
 document.addEventListener("DOMContentLoaded", function () {
     var menu = document.querySelectorAll('nav.lienket');
-    menu = menu[0];
+
+    function myFunction(x) {
+        if (x.matches) { // If media query matches
+            menu = menu[1];
+        } else {
+            menu = menu[0];
+        }
+    }
+
+    var x = window.matchMedia("(max-width: 1023px)")
+    myFunction(x) // Call listener function at run time
     //Truy xuất div menu
     var trangthai = "duoi300";
     window.addEventListener("scroll", function () {
